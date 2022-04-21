d147c11ac34b2f674c72c04197411f71292bfae0
Thiagodgodoy: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 <alunofg@fgacad.edu.br>
2022-04-20 21:22:05 -0300

    segunda att

--- Classes/src/ColorArrays/AddColor.java
+++ Classes/src/ColorArrays/AddColor.java
@@ -1,7 +1,7 @@
 package ColorArrays;
-
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class AddColor {
     static ArrayList<String>color_list = new ArrayList<String>();
@@ -27,6 +27,49 @@ public class AddColor {
         System.out.print("Digite a cor que deseja adicionar: ");
         String color_name = sc.next();
         color_list.add(0, color_name);
+        System.out.println(color_list);
+    }
+    public static void SearchColorId(){
+        System.out.print("Digite qual posição você deseja mostrar: ");
+        int color_id = sc.nextInt();
+        String elements = color_list.get(color_id);
+        System.out.println(elements);
+    }
+    public static void SearchColorName(){
+        System.out.print("Digite qual cor você deseja mostrar: ");
+        String color_title = sc.next();
+        int index = 0;
+        for (int i = 0; i<color_list.size(); i++) {
+           if(color_list.get(i).equals(color_title)) {
+            index = i;               
+           }
+        }
+        System.out.println(index);
+    }
+    public static void RenameColor(){
+        System.out.print("Digite qual gostaria de alterar: ");
+        int color_id = sc.nextInt();
+        System.out.print("Qual cor gostaria de inserir: ");
+        String color_rename = sc.next();
+        String elements = color_list.set(color_id, color_rename);
+        System.out.println(elements);        
+    }
+    public static void RemoveColor(){
+        System.out.println("Digite qual o id da cor que deseja remover: ");
+        int color_id = sc.nextInt();
+        color_list.remove(color_id);
+        System.out.println(color_list);
+    }
+    public static void InsertAtPosition(){
+        System.out.print("Digite a cor que deseja adicionar: ");
+        String color_name = sc.next();
+        System.out.print("Digite o id que deseja adicionar: ");
+        int index = sc.nextInt();
+        color_list.add(index, color_name);
+        System.out.println(color_list);
+    }
+    public static void AlphabeticalSort(){
+        Collections.sort(color_list);
         System.out.println(color_list);
     }
 }
--- Classes/src/ColorArrays/ArraysList.java
+++ Classes/src/ColorArrays/ArraysList.java
@@ -7,7 +7,9 @@ public class ArraysList {
         int choice;
         do{
             System.out.println("Menu Principal");
-            System.out.println("1 - Adicionar cores | 2 - Iterar cores | 3 - Inserir cor na primeira posição");
+            System.out.println("1 - Adicionar cores | 2 - Iterar cores | 3 - Primeira posição");
+            System.out.println("4 - Buscar pelo id | 5 - Buscar pelo nome | 6 - Renomear cor");
+            System.out.println("7 - Remover cor | 8 - Inserir em determinada posição | 9 - Ordem alfabetica");   
             System.out.println("0 - Sair");
             System.out.print("Digite qual opção deseja executar:  ");
             choice = sc.nextInt();
@@ -18,6 +20,12 @@ public class ArraysList {
                 case 1 -> AddColor.Colors();
                 case 2 -> AddColor.Iteract();
                 case 3 -> AddColor.InsertFirst();
+                case 4 -> AddColor.SearchColorId();
+                case 5 -> AddColor.SearchColorName();
+                case 6 -> AddColor.RenameColor();
+                case 7 -> AddColor.RemoveColor();
+                case 8 -> AddColor.InsertAtPosition();
+                case 9 -> AddColor.AlphabeticalSort();
             }
         } while (choice != 0);
         sc.close();
